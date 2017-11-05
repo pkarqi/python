c6e79f189e87ffb73b580a5bf422a0795172d36c
zhixinping <pkarqi@163.com>
2017-11-05 10:59:58 +0800

    function

--- lesson14.py
+++ lesson14.py
@@ -1,4 +1,14 @@
 #coding=utf-8
-def test():
-    return "222"
-print test()
+'''
+def test(a,b,c,d,e):     #位置参数
+    '这是一个测试数据'
+    return "1,2,3,4,5"
+print test(1,2,3,4,5)
+#test.__doc__
+'''
+
+def test1(a=4):
+    'this is test1 function'
+    return a
+print test1()
+print  test1(a=10)
