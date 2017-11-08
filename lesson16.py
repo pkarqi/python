edc09aac10425f06ae972f0c60af22315f3e4f91
zhixinping <pkarqi@163.com>
2017-11-09 00:18:21 +0800

    new

---
+++ lesson16.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+#1、定义一个方法，该方法可以引入任意多的整形参数，结果返回其中最大与最小的值
+
+def func(*num):
+    for i in num:
+        if  not isinstance(i,int):   #判断是否为整正数值
+
+            return "输入的参数必须是整正数"
+    a = sorted(num)
+
+
+    #return "Max is %d and Min is %d" %(a[-1],a[0])
+    return max(num), min(num)
+print func(1,2,3,4,5,6,8,67687,0)
+
+
