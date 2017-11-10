3fe0e73c5251afedf80419dbd39f4be5933ab211
zhixinping <pkarqi@163.com>
2017-11-10 23:36:52 +0800

    new

--- lesson16.py
+++ lesson16.py
@@ -13,24 +13,27 @@ def func(*num):
     #return "Max is %d and Min is %d" %(a[-1],a[0])
     return max(num), min(num)
 print func(1,2,3,4,5,6,8,67687,0)
-
+'''
 #2、定义一个方法，该方法可以引入任意多的字符串参数，结果返回（长度）最长的字符串
 
-def func1(*LEN):
-    for i in LEN:
-        if not isinstance(i,str):
-            return "输入的参数必须是字符串"
-        a = sorted(sen,key=lambda k: len(k), reverse=True)
-
-    return len(a)
-'''
-
-def aaa1(*LEN):
-    StrLen = [ ]
-    for i in LEN:
-            if not isinstance(i,str):
-                return "输入的参数必须是字符串"
-            StrLen.append(len(i))
 
 
-print aaa1('kjkjl','hjkjhkj','UIYYYIUYUIYIUYIUYIU')
+
+def func1(*str):
+    s = list(str)
+    dic = dict([len(x),x] for x in s)
+    return dic[max(dic)]
+print func1('fdfdf','dfderere','123456789','1234567890poiuytrewqasdfg')
+
+
+
+
+
+
+
+
+
+
+
+
+
