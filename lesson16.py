e60ee4dd9cda1419db27f8a9d638da539fa22595
zhixinping <pkarqi@163.com>
2017-11-12 00:15:25 +0800

    new

--- lesson16.py
+++ lesson16.py
@@ -13,7 +13,7 @@ def func(*num):
     #return "Max is %d and Min is %d" %(a[-1],a[0])
     return max(num), min(num)
 print func(1,2,3,4,5,6,8,67687,0)
-'''
+
 #2、定义一个方法，该方法可以引入任意多的字符串参数，结果返回（长度）最长的字符串
 
 
@@ -22,12 +22,26 @@ print func(1,2,3,4,5,6,8,67687,0)
 def func1(*str):
     s = list(str)
     dic = dict([len(x),x] for x in s)
+    print dic
     return dic[max(dic)]
 print func1('fdfdf','dfderere','123456789','1234567890poiuytrewqasdfg')
+#3、定义一个方法get_doc(module)，module参数为该脚本中导入或定义的模块对象，该函数返回module的帮助文档。
+def get_help(a):
+    return help(a)
+print get_help('urllib')
 
+'''
+#4、定义一个方法  f参数为任意一个文件的磁盘路径，该函数返回f文件的内容
+import os
 
+def get_text(str1):
+    if os.path.exists(f):
+        with open(str1,'r') as g:
+            return g.read()
+    else:
+        return 'nothing this file'
 
-
+print get_text('tmp.txt')
 
 
 
