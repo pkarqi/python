872999b37b5b1769995dc9f16b643224c9681757
zhixinping <pkarqi@163.com>
2017-11-19 22:33:30 +0800

    lambda

---
+++ lambda.py
@@ -0,0 +1,16 @@
+#coding=utf-8
+# lambda使用方法
+
+a = [1,2,3,4,5,7]
+b = filter(lambda x :x!=5,a)
+print b
+
+
+
+
+C = [1,2,3,4,5,6,7,8,9]
+D = []
+for i in C:
+    if i !=5:
+        D.append(i)
+print D
