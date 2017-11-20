38769250e28ce1678e9b99cce2472257a77fd528
zhixinping <pkarqi@163.com>
2017-11-20 23:47:09 +0800

    email

---
+++ E-MIAL-CODE1.py
@@ -0,0 +1,32 @@
+#coding=utf-8
+import smtplib
+from email.mime.text import MIMEText
+import string
+
+fp = open('tmp.txt', 'rb')
+msg = MIMEText(fp.read())
+
+fp.close()
+HOST = "smtp.exmail.qq.com"
+SUBJECT = "数据问题"
+TO = "pkarqi@163.com"
+FROM = 'yunwei@zhongchebaolian.com'
+text = '数据问题1111111'
+BODY = string.join(("FROM: %s" % FROM,
+                    "TO: %s" %TO,
+                    "Subject: %s" % SUBJECT,
+
+                    "",
+                    text
+
+                    ),"\r\n")
+
+
+
+server = smtplib.SMTP()
+server.connect(HOST,'25')
+server.starttls()
+server.login("yunwei@zhongchebaolian.com","yW123456")
+server.sendmail(FROM, [TO], BODY, )
+
+server.quit()
